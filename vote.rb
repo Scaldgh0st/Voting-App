532d0bacd5be7dfc1c2ea9690d5b59cfc7705546
Ryan Horricks <ethereal@acidfactory.ca>
2018-10-04 11:22:37 -0600

    Add closed loop for votes

--- vote.rb
+++ vote.rb
@@ -1,9 +1,18 @@
 require 'sinatra'
 
 get '/' do
+  @title = 'Jukebox'
   erb :index
 end
-
+post '/cast'do
+  @title = 'Thanks for voting!'
+  @vote = params['vote']
+  erb :cast
+end
+get '/results' do
+  @votes = { 1 => 10, 2 => 5, 3 => 4, 4 => 2, 5 => 7 }
+  erb :results
+end
 Choices = {
   '1' => 'love',
   '2' => 'like',
